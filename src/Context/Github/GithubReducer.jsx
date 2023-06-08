5328e07ee50ab88737b979308145df6f649060c8
Derrick Maina <mburumaina648@gmail.com>
2023-06-08 14:04:45 +0300

    switched to UseReducers from UseState

---
+++ src/Context/Github/GithubReducer.jsx
@@ -0,0 +1,19 @@
+const githubReducer = (state, action)=>{
+
+   switch(action.type){
+
+      case 'GET_USERS':
+         return {
+            ...state, 
+            users: action.payload
+         }
+            
+         
+      default:
+         return state
+   }
+
+}
+
+
+export default githubReducer
