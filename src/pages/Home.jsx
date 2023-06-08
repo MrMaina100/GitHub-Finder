b5df9c79f2bfc18e8206e6bf4b13a677ff8311e3
Derrick Maina <mburumaina648@gmail.com>
2023-06-08 17:44:33 +0300

    added the search bar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import UsersResults from "../components/results/UsersResults"
+import UseSearch from "../components/results/UseSearch"
+
+
 function Home() {
   return (
     <>
+    <UseSearch/>
     <UsersResults/>
-
-      
+  
     </>
   )
 }
