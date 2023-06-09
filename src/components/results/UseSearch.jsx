0eb2c2546c66f8e81ad31ea2370ff9d19619182e
Derrick Maina <mburumaina648@gmail.com>
2023-06-09 10:42:18 +0300

    added search funtionality

---
+++ src/components/results/UseSearch.jsx
@@ -0,0 +1,72 @@
+import { useState, useContext } from "react"
+import GithubContext from "../../Context/Github/GithubContext";
+
+
+function UseSearch() {
+
+
+   const [text, setText] = useState('');
+
+   const {users, SearchUsers, clearUsers} = useContext(GithubContext);
+
+   const handleSubmit = (e)=>{
+      e.preventDefault();
+
+      if(text === ''){
+         alert('my brother in christ best you input something')
+      }else{
+
+         SearchUsers(text)
+         setText('')
+      }
+
+      
+   }
+
+   const handleClick = (e)=> setText(e.target.value)
+
+  
+
+
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-3 gap-7 mb-8">
+     <div>
+      <form onSubmit={handleSubmit}>
+         <div className="form-control">
+            <div className="relative">
+               <input type="text"
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                 placeholder="Search"
+                 value={text}
+                 onChange={handleClick}
+                 
+                 />
+
+               <button type="submit" className="absolute top-0 right-0  rounded-l-none w-36 btn-lg">
+                  Go
+
+               </button>
+            </div>
+         </div>
+
+      </form>
+
+     </div>
+
+
+     {users.length >0 && (
+
+      <div>
+      <button  onClick={clearUsers} className="btn btn-ghost btn-lg">
+         clear
+
+      </button>
+     </div>
+
+     )}
+
+     
+      </div>
+  )
+}
+export default UseSearch
