9e9e733a0b3cf3ee6fa9bab8ec1c58c8c9facf5a
Derrick Maina <mburumaina648@gmail.com>
2023-06-13 10:40:30 +0300

    added a route

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/about" element={<About/>} />            
-            <Route path="/user" element={<User/>} />            
+            <Route path="/user/:login" element={<User/>} />            
             <Route path="/notfound" element={<NotFound/>} />
             <Route path="/*" element={<NotFound/>} />
           </Routes>
